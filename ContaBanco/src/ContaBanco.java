aa2e51dbd0a57e00d89703ed9569884655051d70
MarcosBassetto <bassetto.marcos@gmail.com>
2024-05-16 08:39:00 -0300

    Ajuste Construtor ContaBanco

--- ContaBanco/src/ContaBanco.java
+++ ContaBanco/src/ContaBanco.java
@@ -8,21 +8,7 @@ public class ContaBanco {
     private Double saldo;
 
 
-    public ContaBanco() {
-
-        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
-
-        System.out.println("Informe por gentileza o número de sua conta:");
-        int numero = sc.nextInt();
-
-        System.out.println("Por favor, digite o número da Agência !");
-        String agencia = sc.next();
-
-        System.out.println("Informe-nos o nome do proprietário da conta:");
-        String nomeCliente = sc.next();
-
-        System.out.println("Informe o valor do saldo atual:");
-        double saldo= sc.nextDouble();
+    public ContaBanco(int numero, String agencia, String nomeCliente, double saldo) {
 
         this.numero = numero;
         this.agencia = agencia;
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        new ContaBanco().imprimeUsuario();
+
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Informe por gentileza o número de sua conta:");
+        int numero = sc.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = sc.next();
+
+        System.out.println("Informe-nos o nome do proprietário da conta:");
+        String nomeCliente = sc.next();
+
+        System.out.println("Informe o valor do saldo atual:");
+        double saldo= sc.nextDouble();
+
+        new ContaBanco(numero, agencia, nomeCliente, saldo).imprimeUsuario();
     }
 }
